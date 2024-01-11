6fada88af8de0e983ca5906ad84871d04b4c75e0
tanmyid <tanmyid1337@gmail.com>
2024-01-11 08:24:46 +0700

    update db

--- db/new_prodi_old.sql
+++ db/new_prodi_old.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Jan 11, 2024 at 01:24 AM
--- Server version: 8.0.30
--- PHP Version: 8.1.10
+-- Host: 127.0.0.1
+-- Generation Time: Jan 06, 2024 at 07:48 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,12 +28,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `barang_in` (
-  `id_barang_in` int NOT NULL,
-  `nama_suplier` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
-  `kategori` int NOT NULL,
-  `nama_barang` int NOT NULL,
+  `id_barang_in` int(11) NOT NULL,
+  `nama_suplier` varchar(30) NOT NULL,
+  `kategori` int(11) NOT NULL,
+  `nama_barang` int(11) NOT NULL,
   `tgl_masuk` date NOT NULL,
-  `qty` int NOT NULL
+  `qty` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -41,16 +41,16 @@ CREATE TABLE `barang_in` (
 --
 
 INSERT INTO `barang_in` (`id_barang_in`, `nama_suplier`, `kategori`, `nama_barang`, `tgl_masuk`, `qty`) VALUES
-(6, 'Bridgestone', 4, 3, '2023-12-15', 169),
-(7, 'bridgestone', 3, 1, '2023-12-19', 175),
-(8, 'bridgestone', 5, 1, '2023-12-19', 615),
-(10, 'dunlop', 3, 11, '2024-01-06', 114),
-(11, 'gajah tunggal', 8, 8, '2024-01-06', 185),
-(12, 'gajah tunggal', 9, 8, '2024-01-06', 134),
-(13, 'gajah tunggal', 10, 8, '2024-01-06', 58),
-(14, 'bridgestone', 10, 10, '2024-01-06', 148),
-(15, 'bridgestone', 9, 10, '2024-01-06', 120),
-(16, 'bridgestone', 8, 9, '2024-01-06', 120),
+(6, 'Bridgestone', 4, 3, '2023-12-15', 138),
+(7, 'bridgestone', 3, 1, '2023-12-19', 50),
+(8, 'bridgestone', 5, 1, '2023-12-19', 70),
+(10, 'dunlop', 3, 11, '2024-01-06', 25),
+(11, 'gajah tunggal', 8, 8, '2024-01-06', 35),
+(12, 'gajah tunggal', 9, 8, '2024-01-06', 50),
+(13, 'gajah tunggal', 10, 8, '2024-01-06', 45),
+(14, 'bridgestone', 10, 10, '2024-01-06', 80),
+(15, 'bridgestone', 9, 10, '2024-01-06', 70),
+(16, 'bridgestone', 8, 9, '2024-01-06', 55),
 (17, 'dunlop', 6, 12, '2024-01-06', 55),
 (18, 'bridgestone', 11, 7, '2024-01-06', 70),
 (19, 'bridgestone', 7, 6, '2024-01-06', 90);
@@ -62,10 +62,10 @@ INSERT INTO `barang_in` (`id_barang_in`, `nama_suplier`, `kategori`, `nama_baran
 --
 
 CREATE TABLE `barang_out` (
-  `id_barang_out` int NOT NULL,
-  `nama_barang` int NOT NULL,
-  `jumlah` int NOT NULL,
-  `pelanggan` int NOT NULL,
+  `id_barang_out` int(11) NOT NULL,
+  `nama_barang` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `pelanggan` int(11) NOT NULL,
   `tanggal_out` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -86,8 +86,8 @@ INSERT INTO `barang_out` (`id_barang_out`, `nama_barang`, `jumlah`, `pelanggan`,
 --
 
 CREATE TABLE `kategori` (
-  `id_kategori` int NOT NULL,
-  `kategori` varchar(30) COLLATE utf8mb4_general_ci NOT NULL
+  `id_kategori` int(11) NOT NULL,
+  `kategori` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -113,8 +113,8 @@ INSERT INTO `kategori` (`id_kategori`, `kategori`) VALUES
 --
 
 CREATE TABLE `nama_barang` (
-  `id_nama_barang` int NOT NULL,
-  `nama_barang` varchar(30) COLLATE utf8mb4_general_ci NOT NULL
+  `id_nama_barang` int(11) NOT NULL,
+  `nama_barang` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -141,10 +141,10 @@ INSERT INTO `nama_barang` (`id_nama_barang`, `nama_barang`) VALUES
 --
 
 CREATE TABLE `pelanggan` (
-  `id_pelanggan` int NOT NULL,
-  `nama_pelanggan` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `no_tlp` varchar(14) COLLATE utf8mb4_general_ci NOT NULL,
-  `plat_no` varchar(30) COLLATE utf8mb4_general_ci NOT NULL
+  `id_pelanggan` int(11) NOT NULL,
+  `nama_pelanggan` varchar(100) NOT NULL,
+  `no_tlp` varchar(14) NOT NULL,
+  `plat_no` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -154,7 +154,7 @@ CREATE TABLE `pelanggan` (
 INSERT INTO `pelanggan` (`id_pelanggan`, `nama_pelanggan`, `no_tlp`, `plat_no`) VALUES
 (1, 'Yanto', '089990009998', 'K 1890 AA'),
 (2, 'Udin', '087778887734', 'K 1890 BB'),
-(4, 'mia ', '087778887734', 'K 6886 HU'),
+(4, 'mia ', '1894671', 'K 6886 HU'),
 (5, 'aditya', '081553723577', 'K 5435 ADB'),
 (6, 'rizal', '08177245667', 'B 1221 UK'),
 (7, 'niko', '089772201983', 'K 3400 JK'),
@@ -167,57 +167,15 @@ INSERT INTO `pelanggan` (`id_pelanggan`, `nama_pelanggan`, `no_tlp`, `plat_no`) 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `stock_barang_in`
---
-
-CREATE TABLE `stock_barang_in` (
-  `id_stok_barang_in` int NOT NULL,
-  `id_barang_in` int NOT NULL,
-  `nama_barang` int NOT NULL,
-  `kategori` int NOT NULL,
-  `tgl_masuk` date NOT NULL,
-  `qty` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `stock_barang_in`
---
-
-INSERT INTO `stock_barang_in` (`id_stok_barang_in`, `id_barang_in`, `nama_barang`, `kategori`, `tgl_masuk`, `qty`) VALUES
-(1, 6, 3, 4, '2024-01-11', 50),
-(2, 6, 3, 4, '2024-01-11', 30),
-(3, 7, 1, 3, '2024-01-11', 100),
-(4, 7, 1, 3, '2024-01-11', 75),
-(5, 8, 1, 5, '2024-01-11', 89),
-(6, 8, 1, 5, '2024-01-11', 526),
-(7, 10, 11, 3, '2024-01-11', 56),
-(8, 10, 11, 3, '2024-01-11', 58),
-(9, 11, 8, 8, '2024-01-11', 98),
-(10, 11, 8, 8, '2024-01-11', 87),
-(11, 12, 8, 9, '2024-01-11', 78),
-(12, 12, 8, 9, '2024-01-11', 56),
-(13, 13, 8, 10, '2024-01-11', 12),
-(14, 13, 8, 10, '2024-01-11', 46),
-(15, 14, 10, 10, '2024-01-11', 85),
-(16, 14, 10, 10, '2024-01-11', 63),
-(17, 15, 10, 9, '2024-01-11', 75),
-(18, 15, 10, 9, '2024-01-11', 45),
-(19, 16, 9, 8, '2024-01-11', 57),
-(20, 16, 9, 8, '2024-01-11', 63),
-(21, 6, 3, 4, '2024-01-11', 89);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
-  `id_user` int NOT NULL,
-  `username` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
-  `nama` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
-  `level` enum('admin','staf','pimpinan') COLLATE utf8mb4_general_ci NOT NULL
+  `id_user` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `level` enum('admin','staf','pimpinan') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -268,15 +226,6 @@ ALTER TABLE `pelanggan`
   ADD PRIMARY KEY (`id_pelanggan`);
 
 --
--- Indexes for table `stock_barang_in`
---
-ALTER TABLE `stock_barang_in`
-  ADD PRIMARY KEY (`id_stok_barang_in`),
-  ADD KEY `barang_in` (`id_barang_in`,`kategori`),
-  ADD KEY `kategori` (`kategori`),
-  ADD KEY `nama_barang` (`nama_barang`);
-
---
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -290,43 +239,37 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `barang_in`
 --
 ALTER TABLE `barang_in`
-  MODIFY `id_barang_in` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id_barang_in` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `barang_out`
 --
 ALTER TABLE `barang_out`
-  MODIFY `id_barang_out` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_barang_out` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id_kategori` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `nama_barang`
 --
 ALTER TABLE `nama_barang`
-  MODIFY `id_nama_barang` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_nama_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
-  MODIFY `id_pelanggan` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT for table `stock_barang_in`
---
-ALTER TABLE `stock_barang_in`
-  MODIFY `id_stok_barang_in` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -345,14 +288,6 @@ ALTER TABLE `barang_in`
 ALTER TABLE `barang_out`
   ADD CONSTRAINT `barang_out_ibfk_2` FOREIGN KEY (`pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `barang_out_ibfk_3` FOREIGN KEY (`nama_barang`) REFERENCES `nama_barang` (`id_nama_barang`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `stock_barang_in`
---
-ALTER TABLE `stock_barang_in`
-  ADD CONSTRAINT `stock_barang_in_ibfk_1` FOREIGN KEY (`id_barang_in`) REFERENCES `barang_in` (`id_barang_in`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `stock_barang_in_ibfk_2` FOREIGN KEY (`kategori`) REFERENCES `kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `stock_barang_in_ibfk_3` FOREIGN KEY (`nama_barang`) REFERENCES `nama_barang` (`id_nama_barang`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
